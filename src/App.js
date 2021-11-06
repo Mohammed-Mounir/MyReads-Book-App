448b5377edf84a1000df9a88f2cc6885d4a27b37
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2021-11-06 11:38:46 +0200

    feat: Added react-router-dom and moved search into its own component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import BookShelves from "./BookShelves";
+import BooksSearch from "./BooksSearch";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: [],
     bookShelves: [
       { shelfKey: "currentlyReading", shelfTitle: "Currently Reading" },
       { shelfKey: "wantToRead", shelfTitle: "Want to Read" },
       { shelfKey: "read", shelfTitle: "Read" },
     ],
-    showSearchPage: false,
   };
 
   componentDidMount() {
@@ -57,55 +52,37 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <Routes>
+          <Route path="/search" element={<BooksSearch />} />
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
+          <Route
+            path="/"
+            element={
+              <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                  <div>
+                    {this.state.bookShelves.map((shelf) => (
+                      <BookShelves
+                        key={shelf.shelfKey}
+                        shelf={shelf}
+                        books={this.state.books}
+                        onSelectChange={this.handleSelectChange}
+                      />
+                    ))}
+                  </div>
+                </div>
+                <div className="open-search">
+                  <Link to="/search" className="btn-search">
+                    Add a book
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {this.state.bookShelves.map((shelf) => (
-                  <BookShelves
-                    key={shelf.shelfKey}
-                    shelf={shelf}
-                    books={this.state.books}
-                    onSelectChange={this.handleSelectChange}
-                  />
-                ))}
-              </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </button>
-            </div>
-          </div>
-        )}
+            }
+          />
+        </Routes>
       </div>
     );
   }
